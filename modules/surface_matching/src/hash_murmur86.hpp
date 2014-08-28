5d1087a81b34b805362cfa5b4f6480ab423f0f90
tolgabirdal <tbirdal@gmail.com>
2014-08-29 01:19:45 +0300

    reformatted

--- modules/surface_matching/src/hash_murmur86.hpp
+++ modules/surface_matching/src/hash_murmur86.hpp
@@ -21,7 +21,7 @@
 
 /* If the compiler says it's C99 then take its word for it */
 #if !defined(MH_UINT32) && ( \
-	defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L )
+ defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L )
 #include <stdint.h>
 #define MH_UINT32 uint32_t
 #endif
@@ -137,28 +137,28 @@ MH_UINT32 PMurHash32(MH_UINT32 seed, const void *key, int len);
 /* This is the main processing body of the algorithm. It operates
 * on each full 32-bits of input. */
 #define DOBLOCK(h1, k1) do{ \
-	k1 *= C1; \
-	k1 = ROTL32(k1,15); \
-	k1 *= C2; \
-	\
-	h1 ^= k1; \
-	h1 = ROTL32(h1,13); \
-	h1 = h1*5+0xe6546b64; \
-}while(0)
+ k1 *= C1; \
+      k1 = ROTL32(k1,15); \
+           k1 *= C2; \
+                 \
+                 h1 ^= k1; \
+                       h1 = ROTL32(h1,13); \
+                            h1 = h1*5+0xe6546b64; \
+                                 }while (0)
 
 
 /* Append unaligned bytes to carry, forcing hash churn if we have 4 bytes */
 /* cnt=bytes to process, h1=name of h1 var, c=carry, n=bytes in c, ptr/len=payload */
 #define DOBYTES(cnt, h1, c, n, ptr, len) do{ \
-	int _i = cnt; \
-	while(_i--) { \
-	c = c>>8 | *ptr++<<24; \
-	n++; len--; \
-	if(n==4) { \
-	DOBLOCK(h1, c); \
-	n = 0; \
-	} \
-	} }while(0)
+                                  int _i = cnt; \
+                                                                           while (_i--){    \
+        c = c>>8 | *ptr++<<24;    \
+        n++;    len--;    \
+        if (n==4)        {            \
+                        DOBLOCK(h1, c);            \
+                        n = 0;            \
+                    } \
+            }}while (0)
 
 /*---------------------------------------------------------------------------*/
 
@@ -166,107 +166,120 @@ MH_UINT32 PMurHash32(MH_UINT32 seed, const void *key, int len);
 * if wanted. Both ph1 and pcarry are required arguments. */
 void PMurHash32_Process(uint32_t *ph1, uint32_t *pcarry, const void *key, int len)
 {
-	uint32_t h1 = *ph1;
-	uint32_t c = *pcarry;
-
-	const uint8_t *ptr = (uint8_t*)key;
-	const uint8_t *end;
-
-	/* Extract carry count from low 2 bits of c value */
-	int n = c & 3;
-
+    uint32_t h1 = *ph1;
+    uint32_t c = *pcarry;
+    
+    const uint8_t *ptr = (uint8_t*)                         key;
+    const uint8_t *end;
+    
+    /* Extract carry count from low 2 bits of c value */
+    int n = c & 3;
+    
 #if defined(UNALIGNED_SAFE)
-	/* This CPU handles unaligned word access */
-
-	/* Consume any carry bytes */
-	int i = (4-n) & 3;
-	if(i && i <= len) {
-		DOBYTES(i, h1, c, n, ptr, len);
-	}
-
-	/* Process 32-bit chunks */
-	end = ptr + len/4*4;
-	for( ; ptr < end ; ptr+=4) {
-		uint32_t k1 = READ_UINT32(ptr);
-		DOBLOCK(h1, k1);
-	}
-
+    /* This CPU handles unaligned word access */
+    
+    /* Consume any carry bytes */
+    int i = (4-n) & 3;
+    if (i && i <= len)
+    {
+        DOBYTES(i, h1, c, n, ptr, len);
+    }
+    
+    /* Process 32-bit chunks */
+    end = ptr + len/4*4;
+    for ( ; ptr < end ; ptr+=4)
+    {
+        uint32_t k1 = READ_UINT32(ptr);
+        DOBLOCK(h1, k1);
+    }
+    
 #else /*UNALIGNED_SAFE*/
-	/* This CPU does not handle unaligned word access */
-
-	/* Consume enough so that the next data byte is word aligned */
-	int i = -(long)ptr & 3;
-	if(i && i <= len) {
-		DOBYTES(i, h1, c, n, ptr, len);
-	}
-
-	/* We're now aligned. Process in aligned blocks. Specialise for each possible carry count */
-	end = ptr + len/4*4;
-	switch(n) { /* how many bytes in c */
-  case 0: /* c=[----]  w=[3210]  b=[3210]=w            c'=[----] */
-	  for( ; ptr < end ; ptr+=4) {
-		  uint32_t k1 = READ_UINT32(ptr);
-		  DOBLOCK(h1, k1);
-	  }
-	  break;
-  case 1: /* c=[0---]  w=[4321]  b=[3210]=c>>24|w<<8   c'=[4---] */
-	  for( ; ptr < end ; ptr+=4) {
-		  uint32_t k1 = c>>24;
-		  c = READ_UINT32(ptr);
-		  k1 |= c<<8;
-		  DOBLOCK(h1, k1);
-	  }
-	  break;
-  case 2: /* c=[10--]  w=[5432]  b=[3210]=c>>16|w<<16  c'=[54--] */
-	  for( ; ptr < end ; ptr+=4) {
-		  uint32_t k1 = c>>16;
-		  c = READ_UINT32(ptr);
-		  k1 |= c<<16;
-		  DOBLOCK(h1, k1);
-	  }
-	  break;
-  case 3: /* c=[210-]  w=[6543]  b=[3210]=c>>8|w<<24   c'=[654-] */
-	  for( ; ptr < end ; ptr+=4) {
-		  uint32_t k1 = c>>8;
-		  c = READ_UINT32(ptr);
-		  k1 |= c<<24;
-		  DOBLOCK(h1, k1);
-	  }
-	}
+    /* This CPU does not handle unaligned word access */
+    
+    /* Consume enough so that the next data byte is word aligned */
+    int i = -(long)ptr & 3;
+    if (i && i <= len)
+    {
+        DOBYTES(i, h1, c, n, ptr, len);
+    }
+    
+    /* We're now aligned. Process in aligned blocks. Specialise for each possible carry count */
+    end = ptr + len/4*4;
+    switch (n)
+    {
+            /* how many bytes in c */
+        case 0: /* c=[----]  w=[3210]  b=[3210]=w            c'=[----] */
+            for ( ; ptr < end ; ptr+=4)
+            {
+                uint32_t k1 = READ_UINT32(ptr);
+                DOBLOCK(h1, k1);
+            }
+            break;
+        case 1: /* c=[0---]  w=[4321]  b=[3210]=c>>24|w<<8   c'=[4---] */
+            for ( ; ptr < end ; ptr+=4)
+            {
+                uint32_t k1 = c>>24;
+                c = READ_UINT32(ptr);
+                k1 |= c<<8;
+                DOBLOCK(h1, k1);
+            }
+            break;
+        case 2: /* c=[10--]  w=[5432]  b=[3210]=c>>16|w<<16  c'=[54--] */
+            for ( ; ptr < end ; ptr+=4)
+            {
+                uint32_t k1 = c>>16;
+                c = READ_UINT32(ptr);
+                k1 |= c<<16;
+                DOBLOCK(h1, k1);
+            }
+            break;
+        case 3: /* c=[210-]  w=[6543]  b=[3210]=c>>8|w<<24   c'=[654-] */
+            for ( ; ptr < end ; ptr+=4)
+            {
+                uint32_t k1 = c>>8;
+                c = READ_UINT32(ptr);
+                k1 |= c<<24;
+                DOBLOCK(h1, k1);
+            }
+    }
 #endif /*UNALIGNED_SAFE*/
-
-	/* Advance over whole 32-bit chunks, possibly leaving 1..3 bytes */
-	len -= len/4*4;
-
-	/* Append any remaining bytes into carry */
-	DOBYTES(len, h1, c, n, ptr, len);
-
-	/* Copy out new running hash and carry */
-	*ph1 = h1;
-	*pcarry = (c & ~0xff) | n;
-} 
+    
+    /* Advance over whole 32-bit chunks, possibly leaving 1..3 bytes */
+    len -= len/4*4;
+    
+    /* Append any remaining bytes into carry */
+    DOBYTES(len, h1, c, n, ptr, len);
+    
+    /* Copy out new running hash and carry */
+    *ph1 = h1;
+    *pcarry = (c & ~0xff) | n;
+}
 
 /*---------------------------------------------------------------------------*/
 
 /* Finalize a hash. To match the original Murmur3A the total_length must be provided */
 uint32_t PMurHash32_Result(uint32_t h, uint32_t carry, uint32_t total_length)
 {
-	uint32_t k1;
-	int n = carry & 3;
-	if(n) {
-		k1 = carry >> (4-n)*8;
-		k1 *= C1; k1 = ROTL32(k1,15); k1 *= C2; h ^= k1;
-	}
-	h ^= total_length;
-
-	/* fmix */
-	h ^= h >> 16;
-	h *= 0x85ebca6b;
-	h ^= h >> 13;
-	h *= 0xc2b2ae35;
-	h ^= h >> 16;
-
-	return h;
+    uint32_t k1;
+    int n = carry & 3;
+    if (n)
+    {
+        k1 = carry >> (4-n)*8;
+        k1 *= C1;
+        k1 = ROTL32(k1,15);
+        k1 *= C2;
+        h ^= k1;
+    }
+    h ^= total_length;
+    
+    /* fmix */
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
+    
+    return h;
 }
 
 /*---------------------------------------------------------------------------*/
@@ -274,13 +287,14 @@ uint32_t PMurHash32_Result(uint32_t h, uint32_t carry, uint32_t total_length)
 /* Murmur3A compatable all-at-once */
 uint32_t PMurHash32(uint32_t seed, const void *key, int len)
 {
-	uint32_t h1=seed, carry=0;
-	PMurHash32_Process(&h1, &carry, key, len);
-	return PMurHash32_Result(h1, carry, len);
+    uint32_t h1=seed, carry=0;
+    PMurHash32_Process(&h1, &carry, key, len);
+    return PMurHash32_Result(h1, carry, len);
 }
 
 void hashMurmurx86 ( const void * key, const int len, const unsigned int seed, void * out )
 {
-	const unsigned long tempSeed = (unsigned long)seed;
-	*(unsigned int*)out = PMurHash32 (seed, key, len);
+    const unsigned long tempSeed = (unsigned long)seed;
+    *(unsigned int*)out = PMurHash32 (seed, key, len);
 }
+
